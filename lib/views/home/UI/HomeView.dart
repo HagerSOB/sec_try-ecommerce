03e1ec8756894ac9f3e3223b7e36edfaa7d0cc68
???? ??? <????? ?????????? ???>
2026-02-27 22:31:38 +0200

    add Icon categories

--- lib/views/home/UI/HomeView.dart
+++ lib/views/home/UI/HomeView.dart
@@ -14,8 +14,43 @@ class HomeView extends StatelessWidget {
         children: [
           CustomSearchField(),
           SizedBox(height: 20,),
-          Image.asset("assets/images/shopping.jpg",)
-          ]),
+          Image.asset("assets/images/shopping.jpg",),
+          SizedBox(height: 20,),
+
+          Text("Popular categories",style: TextStyle(fontSize: 24,fontWeight: FontWeight.normal),)
+          ,SizedBox(height: 15,),
+          SizedBox(
+            height: 65,
+            child: ListView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+                itemCount: categories.length,
+                itemBuilder: (context,index){
+                  return CircleAvatar(
+                    backgroundColor: AppColors.kPrimaryColor,
+                    foregroundColor: AppColors.kWhiteColor,
+                    child: Icon(categories[index].icon),
+                  );
+                },
+                ),
+          )
+
+        ]),
     );
   }
+}
+List<category> categories=[
+  category(icon: Icons.sports, text: "sports")
+  ,category(icon: Icons.tv, text: "elctronices")
+  ,category(icon: Icons.collections, text: "collections")
+  ,category(icon: Icons.book, text: "books")
+  ,category(icon: Icons.games, text: "games")
+];
+
+class category{
+  final IconData icon;
+  final String text;
+
+  category({required this.icon, required this.text});
+
 }
